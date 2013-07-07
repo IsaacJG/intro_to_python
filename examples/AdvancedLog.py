0011b3e24d153b6e433dfbdf749c53684e690052
Isaac Grant <waterwolf2.0@gmail.com>
2013-07-07 12:36:54 -0600

    removed unnecessary files

--- examples/AdvancedLog.py
+++
@@ -1,8 +0,0 @@
-log_file = 'log.txt'
-
-if __name__ == '__main__':
-	with open(log_file, 'w') as log:
-		stuff = input('log: ')
-		while not stuff == 'exit':
-			log.write(stuff + '\n')
-			stuff = input('log: ')
